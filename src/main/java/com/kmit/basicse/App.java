380ec8909fa4425390125fbf6a1f78f321f2f963
vamsi <vamsichowdary2002@gmail.com>
2022-12-16 10:04:30 +0530

    hello

---
+++ src/main/java/com/kmit/basicse/App.java
@@ -0,0 +1,13 @@
+package com.kmit.basicse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
